a43566d3b62c5eb0a66866df16af9693c041f750
Vernizze <cfvernizze@gmail.com>
2020-12-27 21:49:28 -0300

    Ajuste Response

--- Vrnz2.BaseContracts/DTOs/Base/BaseDTO.cs
+++ Vrnz2.BaseContracts/DTOs/Base/BaseDTO.cs
@@ -12,6 +12,7 @@ namespace Vrnz2.BaseContracts.DTOs.Base
         public abstract class Response<T>
         {
             public virtual bool Success { get; set; }
+            public virtual int? StatusCode { get; set; }
             public virtual string Message { get; set; }
             public virtual DateTime ReturnDateTime { get; set; } = DateTime.UtcNow;
             public virtual T Content { get; set; }
